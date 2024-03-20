a72cd9927116798221c8b5c4ccd8e6514ff1ec65
Stelios Papadakis <spapadakis92@gmail.com>
2024-03-20 17:04:30 +0100

    Added and integrated css and html

--- TitanicProject/TitanicProject/settings.py
+++ TitanicProject/TitanicProject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware', 
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -115,8 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os.paths
 STATIC_URL = 'static/'
+STATICFILES_DIRS=(os.path.join('static'),)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
